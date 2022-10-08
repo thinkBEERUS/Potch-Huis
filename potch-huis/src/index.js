8692d6391e1b57249ac4659c2517741da3e04c1a
thinkBEERUS <99795630+thinkBEERUS@users.noreply.github.com>
2022-10-08 16:14:52 +0200

    Build

--- potch-huis/src/index.js
+++ potch-huis/src/index.js
@@ -13,11 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <HashRouter basename={"https://thinkbeerus.github.io/Poth-Huis"}>
     <Routes>
-      <Route index element={<Login />} />
-      <Route path="Login" element={<Login />} />
-      <Route path="Register" element={<Register />} />
-      <Route path="Dashboard" element={<Dashboard />} />
-      <Route path="Reports" element={<Reports />} />
+      <Route index path="/Login" element={<Login />} />
+      <Route path="/Login" element={<Login />} />
+      <Route path="/Register" element={<Register />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/Reports" element={<Reports />} />
     </Routes>
   </HashRouter>
 );
