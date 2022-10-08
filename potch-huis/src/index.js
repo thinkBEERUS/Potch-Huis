616cd02409ad0c55885887b70ad440596c963719
thinkBEERUS <99795630+thinkBEERUS@users.noreply.github.com>
2022-10-08 17:32:24 +0200

    Build

--- potch-huis/src/index.js
+++ potch-huis/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import './index.css';
 import Login from './Auth/Login';
 import Register from './Auth/Register';
@@ -11,7 +11,7 @@ import Reports from './Reports/Reports';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route index element={<Login />} />
       <Route path="/Login" element={<Login />} />
@@ -19,5 +19,5 @@ root.render(
       <Route path="/Dashboard" element={<Dashboard />} />
       <Route path="/Reports" element={<Reports />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
